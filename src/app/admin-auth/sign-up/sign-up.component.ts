f30bbf423e98e75ab2642c13167e8d8efa4eaf7e
kriti-123 <ks11115500@gmail.com>
2023-12-27 18:40:51 +0530

    first-commit

---
+++ src/app/admin-auth/sign-up/sign-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-admin',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+  //  color="red"
+  days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { AdminAuthModule } from './admin-auth/admin-auth.module';
+import { UserAuthComponent } from './register/user-auth/user-auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule,
+    AdminAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
